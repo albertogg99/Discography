bfa9ebac2d9002bded2e963e5ec6ae3537009b2c
Alex <toribio.17197@gmail.com>
2021-04-20 22:55:00 +0200

    Incorporado test unitario (checkeo de hash)

--- app/src/test/java/com/uc3m/discography/ExampleUnitTest.kt
+++ app/src/test/java/com/uc3m/discography/ExampleUnitTest.kt
@@ -1,8 +1,13 @@
 package com.uc3m.discography
 
+import android.util.Log
+import com.uc3m.discography.model.User
+import org.junit.After
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
+import com.uc3m.discography.util.Hasher
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +15,34 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    lateinit var user: User
+    @Before
+    fun setUp(){
+        val pass = "1234"
+        val hasher = Hasher()
+        val salt = hasher.generateSalt(64)
+        val hashedPass = hasher.hashPassword(pass, salt)
+        val passToStore = ByteArray(salt.size + hashedPass.size)
+        for (i in salt.indices){
+            passToStore[i] = salt[i]
+        }
+        for (i in hashedPass.indices){
+            passToStore[i+64] = hashedPass[i]
+        }
+        user = User ("alex@gmail.com","Alejandro","Fernandez",passToStore)
+
     }
+    @Test
+    fun loginhashTest() {
+        val h = Hasher()
+        val salt = ByteArray(64)
+        for (i in salt.indices){
+            salt[i] = user.password[i]
+        }
+        val hashedPass = h.hashPassword("1234", salt)
+        for (i in hashedPass.indices) {
+            assertSame(user.password[i + 64], hashedPass[i])
+        }
+    }
+
 }
